b8b6f1902000b99d67022a25c9c97367aad34a21
Jersson Moros <jersson0423@gmail.com>
2023-04-17 08:47:56 -0500

    Changes and updates!

---
+++ src/containers/pages/home.jsx
@@ -0,0 +1,18 @@
+import Layout from "../../hocs/layouts/Layout"  
+import Navbar from "components/navigation/Navbar"
+import Footer from "components/navigation/Footer"
+
+function Home(){
+    return(
+        <Layout>
+            <Navbar/>
+            <div className="pt-28">
+                fdsfnjdsnfjsdnf
+                
+            </div>
+            <Footer/>
+            
+        </Layout>
+    )
+}
+export default Home
